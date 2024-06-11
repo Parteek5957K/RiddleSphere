839efe91f5f190976eed1151e648aea01f87973f
Guryansh Singla <108322368+Guryansh@users.noreply.github.com>
2024-06-11 15:08:09 +0530

    Update settings.py

--- riddlesphere/settings.py
+++ riddlesphere/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'riddlesphere.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'qtveYpuApGTydwlpdXumlttLpVWMjAIo',
+        'HOST':'roundhouse.proxy.rlwy.net',
+        'PORT':'50913',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
