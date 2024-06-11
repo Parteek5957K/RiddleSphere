6cf01cd8638a3e14ec8afab31ded1734bfaa09e4
Guryansh Singla <108322368+Guryansh@users.noreply.github.com>
2024-06-11 15:01:37 +0530

    Update settings.py

--- riddlesphere/settings.py
+++ riddlesphere/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q11lxr1xa#*+q@(gk*lp)!g$4x4ab36u2a=7c9gdot!tc2=_z2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost']
 
 
 # Application definition
@@ -123,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 
 MEDIA_ROOT= BASE_DIR / 'static/images'
 
