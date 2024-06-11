5b94088d42c8f27244dac88ed95e06dddc8afcda
Guryansh Singla <108322368+Guryansh@users.noreply.github.com>
2024-06-11 19:38:12 +0530

    Update urls.py

--- riddlesphere/urls.py
+++ riddlesphere/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('', include('room.urls')),
-path('puzzle/', include('puzzle.urls')),
+    path('puzzle/', include('puzzle.urls')),
+    path('<path:incorrect_url>/', TemplateView.as_view(template_name='error.html'), name='incorrect_url'),
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
